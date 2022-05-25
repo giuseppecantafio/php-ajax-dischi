a16761d1eb78fb52041d3c8d6986ff95e23888d9
Giuseppe <giuseppecantafio@hotmail.it>
2022-05-25 18:21:04 +0200

    bonus concluso

--- script/script.js
+++ script/script.js
@@ -2,14 +2,36 @@ const app = new Vue({
     'el': '#app',
     data: {
         dischi: [],
-    },
-    methods: {
+        selectedGenre: '',
+        valueForSelect: [],
+        apiPath: './server.php/',
+        path: '',
 
     },
+    methods: {
+        mySearch() {
+            this.getData(this.selectedGenre);
+        },
+        getData(genre = null) {
+            if (genre) {
+                this.path = `${this.apiPath}?genre=${this.selectedGenre}`;
+            } else {
+                this.path = `${this.apiPath}`;
+            }
+            axios.get(this.path).then((response) => {
+                response.data;
+                this.dischi = response.data;
+                if (this.valueForSelect < 1) {
+                    this.dischi.forEach((disco) => {
+                        if (!this.valueForSelect.includes(disco.genre)) {
+                            this.valueForSelect.push(disco.genre);
+                        }
+                    })
+                }
+            })
+        }
+    },
     mounted() {
-        axios.get('server.php').then((response) => {
-            response.data;
-            this.dischi = response.data;
-        })
+        this.getData();
     }
 })
--- server.php
+++ server.php
@@ -1,8 +1,20 @@
 <?php
 include __DIR__ . '/database.php';
 
+$filteredByGenre = [];
+
+if (isset($_GET['genre'])) {
+    foreach ($dischi as $disco){
+        if ($disco['genre'] == $_GET['genre']){
+            $filteredByGenre[] = $disco;
+        }
+    } 
+} else {
+
+    $filteredByGenre = $dischi;
+}
 header('Content-Type: application/json');
 
-echo json_encode($dischi);
+echo json_encode($filteredByGenre);
 
 ?>
